c17ce41aa88128d6b2fe03f12a66938ac5230911
Shubham Sharma <shubham.sharma200121@gmail.com>
2022-01-12 00:19:58 +0530

    Intial Commit

---
+++ app/src/main/java/com/sharma/growb/dashBoard.kt
@@ -0,0 +1,11 @@
+package com.sharma.growb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dashBoard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash_board)
+    }
+}
